56b14c3d52b8bdbf404b81a95f729592d6f83479
shshnn <ssss5489@naver.com>
2021-07-12 09:51:56 +0900

    프로젝트 추가

--- src/gitpro/hello2.java
+++ src/gitpro/hello2.java
@@ -5,7 +5,7 @@ public class hello2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("hello2-project");
+		System.out.println("hello2 project");
 	}
 
 }
